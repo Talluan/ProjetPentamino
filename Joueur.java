80bb63f58e2e89e7fbbbb7c6a9aafec950fb68d9
Baptiste <bluebapt@gmail.com>
2021-03-24 16:25:15 +0100

    commit baptiste

---
+++ Joueur.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.io.*;
+public abstract class Joueur implements Serializable{
+    private String nom;
+    private double scoreMoyen;
+    private ArrayList<int> liste;
+
+    public Joueur chargerJoueur(String nom){
+        
+    }
+
+    
+}
